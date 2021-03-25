07cfb48b285de7744e8f2abf6d59f3d37536f84a
Zolds403 <51422376+Zolds403@users.noreply.github.com>
2021-03-24 22:55:14 -0400

    Add files via upload
    npm bootstrap

---
+++ cv-master/script.js
@@ -0,0 +1,6 @@
+const followBtn = document.getElementById('btn btn-primary');
+const MessageBtn = document.getElementById('btn btn-outline-primary');
+
+followBtn.onclick = function(){
+    location.replace("https://linkedin.com/in/zsone-olds/");
+};
